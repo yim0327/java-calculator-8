e2d84e84daf2d56df366f6933a1ece064513c745
yim0327 <yim0327@naver.com>
2025-10-16 14:15:00 +0900

    feat(inputview): 사용자 문자열 입력 구현
    - InputView의 inputString()을 통해 사용자 문자열 입력 구현
    - Application은 객체 선언 및 생성만 수행
    - CalculateManager가 콘솔 I/O와 service간 흐름 제어를 담당

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import calculator.controller.CalculateManager;
+import calculator.service.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Calculator calculator = new Calculator();
+        CalculateManager calculateManager = new CalculateManager(inputView, outputView, calculator);
+
+        calculateManager.start();
     }
 }
--- src/main/java/calculator/controller/CalculateManager.java
+++ src/main/java/calculator/controller/CalculateManager.java
@@ -1,4 +1,20 @@
 package calculator.controller;
 
+import calculator.service.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class CalculateManager {
+    private InputView inputView;
+    private OutputView outputView;
+
+    public CalculateManager(InputView inputView, OutputView outputView, Calculator calculator) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        inputView.inputString();
+    }
+
 }
