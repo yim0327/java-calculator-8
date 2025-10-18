2804d67043281faa45ce28697dcfbe71ce24cb2d
yim0327 <yim0327@naver.com>
2025-10-18 21:45:51 +0900

    feat: start 로직 구현 및 결과 출력문 추가
    - CalculateManager 객체의 서비스 흐름 요청 start 메서드 구현
    - OutputView에 결과 출력용 printResult 메서드 구현

--- src/main/java/calculator/controller/CalculateManager.java
+++ src/main/java/calculator/controller/CalculateManager.java
@@ -5,16 +5,19 @@ import calculator.view.InputView;
 import calculator.view.OutputView;
 
 public class CalculateManager {
-    private InputView inputView;
-    private OutputView outputView;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Calculator calculator;
 
     public CalculateManager(InputView inputView, OutputView outputView, Calculator calculator) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.calculator = calculator;
     }
 
     public void start() {
-        inputView.inputString();
+        int result = calculator.calculate(inputView.inputString());
+        outputView.printResult(result);
     }
 
 }
