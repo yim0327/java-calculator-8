c331d793fb0ecb1800300f78e5424d11c3829721
yim0327 <yim0327@naver.com>
2025-10-18 19:58:10 +0900

    feat(numberlist): NumberList 일급 컬렉션 정의
    - Parser에 의해 추출된 ParsedNumber 객체들을 다루기 위한 일급 컬렉션
      정의
    - 불변 리스트를 통한 안전한 데이터 관리 및 일관적인 규칙 도입 확보
    - getNumberList()를 통해 안전한 복사 처리

--- src/main/java/calculator/domain/NumberList.java
+++ src/main/java/calculator/domain/NumberList.java
@@ -3,6 +3,14 @@ package calculator.domain;
 import java.util.List;
 
 public class NumberList {
-    public NumberList(List<ParsedNumber> parsedList) {}
+    private final List<ParsedNumber> numberList;
+
+    public NumberList(List<ParsedNumber> parsedList) {
+        this.numberList = parsedList;
+    }
+
+    public List<ParsedNumber> getNumberList() {
+        return List.copyOf(numberList);
+    }
 
 }
