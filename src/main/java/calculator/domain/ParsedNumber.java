88f3e172ab9bff2c27efa1612ad22eeea5e474c9
yim0327 <yim0327@naver.com>
2025-10-19 01:58:21 +0900

    feat(parsednumber): 입력 문자열 양수 검증 로직 추가
    - parsedNumber의 정적 팩토리 메서드 내에서 객체 생성 전, 양수 검증 로직
      추가
      - 구현 요구사항에 따라 음수나 문자 등이 혼재 시 예외 처리

--- src/main/java/calculator/domain/ParsedNumber.java
+++ src/main/java/calculator/domain/ParsedNumber.java
@@ -8,6 +8,10 @@ public class ParsedNumber {
     }
 
     public static ParsedNumber from(String parsedString) {
+        if (!parsedString.matches("[0-9]+")) {
+            throw new NumberFormatException("문자열은 구분자를 제외하고 양수만 입력 가능합니다.");
+        }
+
         return new ParsedNumber(Integer.parseInt(parsedString));
     }
 
