953a828b435a4583c685fd93defb19d5e9897a7c
yim0327 <yim0327@naver.com>
2025-10-19 15:19:19 +0900

    style(inputview): 불필요한 개행 출력 제거

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public String inputString() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.\n");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 }
